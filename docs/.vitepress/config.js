38cedf19a2ddf41d35d9bccf9647ee7f14276837
Loveyless <88988179+Loveyless@users.noreply.github.com>
2022-06-20 04:23:23 +0800

    build on error

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,25 +1,81 @@
 export default {
   // 将在其中部署计划站点https://foo.github.io/bar/的URL '/bar/' 斜线结尾
-  base: '/docs/',
-  title: 'Loveyless的笔记', //站点标题
+  // base: '/docs/',
+  title: 'Loveyless', //站点标题
   description: 'Loveyless的笔记网站', // 网站的描述。这将呈现为<meta>页面 HTML 中的标签。
   lang: 'zh-CN',
   lastUpdated: true, //最近更新时间
   themeConfig: {
-    nav: [ //上方导航
-      { text: '主页', link: '/index' },
-      // { text: 'xxx', link: 'xxx' }
-    ],
-    sidebar: [ //侧边栏
-      {
-        text: '框架库笔记',
-        items: [
-          { text: 'vue3', link: '/framework/天禹vue3笔记/天禹vue3笔记.md' }
-        ],
-        collapsible: true, //折叠
-        collapsed: true, //默认打开？
+    // siteTitle: 'My Custom Title',
+    algolia: {
+      appKey: '',
+      indexName: '',
+      searchParameters: {
+        faeFilters: ['tags:vue,react']
       }
-    ]
-
+    },
+    nav: [
+      {
+        text: '学习笔记',
+        // activeMatch: '/vue/', //这个还没懂有下拉的怎么搞
+        // activeMatch: `*/framework/*`,
+        items: [
+          {
+            text: '框架', items: [
+              { text: 'Vue', link: '/framework/vue/index.md', },
+              { text: 'React', link: '/framework/react/index.md', },
+            ]
+          }, {
+            text: '后台', items: [
+              { text: 'Express', link: '/backend/express/index.md', },
+              { text: 'MySql', link: '/backend/mysql/index.md', },
+            ]
+          }
+        ]
+      }
+    ],
+    sidebar: {
+      '/framework/vue/': [
+        {
+          text: '开始',
+          items: [
+            { text: '简介', link: '/framework/vue/index.md' },
+          ]
+        },
+        {
+          text: '基础',
+          items: [
+            { text: 'test', link: '/framework/vue/test.md' },
+          ]
+        }
+      ],
+      '/framework/react/': [
+        {
+          text: '开始',
+          items: [
+            { text: 'index', link: '/framework/react/index.md' },
+          ]
+        },
+        {
+          text: 'xxx',
+          items: [
+            { text: 'xxx', link: '/framework/react/xxx.md' },
+          ]
+        }
+      ],
+      '/backend/express/': [
+        { text: 'index', link: '/backend/express/index.md' }
+      ],
+      '/backend/mysql/': [
+        { text: 'index', link: '/backend/mysql/index.md' }
+      ]
+    },
+    socialLinks: [ //社交帐号的logo 您可以自定义socialLinks选项以显示图标的社交帐户链接。
+      { icon: 'github', link: 'https://github.com/Loveyless' },
+    ],
+    editLink: { //在 GitHub 上编辑此页面
+      text: '在 GitHub 上编辑此页面',
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path'
+    },
   }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,16 @@
-## hello world
+---
+layout: home
+
+hero:
+  name: Loveyless
+  text: 学习笔记
+  tagline: Lorem ipsum...
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/what-is-vitepress
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/Loveyless
+---
+
